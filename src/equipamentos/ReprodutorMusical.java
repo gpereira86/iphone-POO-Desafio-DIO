51fd4da21a82b3fa7830ae7443cf0b5923735142
Glauco Pereira <125097992+gpereira86@users.noreply.github.com>
2024-10-25 12:15:37 -0300

    Add files via upload

---
+++ src/equipamentos/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package iphone.equipamentos;
+
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
---
+++ src/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package iphone.usuario;
+
+import iphone.equipamentos.Iphone;
+
+public class Usuario {
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+        
+        // Teste ReprodutorMusical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        
+        // Teste AparelhoTelefonico
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+        
+        // Teste NavegadorInternet
+        meuIphone.exibirPagina("www.openai.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
